6939c16dbfc4d297dc7a200053c0c20ff63e198f
Irina178 <Irina178@users.noreply.github.com>
2022-04-05 21:17:13 +0300

    5

--- js/script.js
+++ js/script.js
@@ -30,10 +30,25 @@ console.log((speedOfSecond * time) - (speedOfFirst * time));
 
 //Задание 4
 const randomNumber = Math.floor(Math.random() * 100);
-if (randomNumber < 20) {
+
+if ("randomNumber < 20") {
    console.log("randomNumber меньше 20")
 } else if (randomNumber > 50) {
    console.log("randomNumber больше 50")
 } else {
    console.log("randomNumber больше 20, и меньше 50")
+}
+
+//Задание 5
+let num;
+switch (randomNumber) {
+   case "randomNumber меньше 20":
+      num = randomNumber < 20;
+      break;
+
+   case "randomNumber больше 50":
+      num = randomNumber > 50
+      break;
+   default:
+      "randomNumber больше 20, и меньше 50"
 }
