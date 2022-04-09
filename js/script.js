438304b1b3e01d3bbda363e457960a05f7abbf7a
Irina178 <Irina178@users.noreply.github.com>
2022-04-09 19:47:32 +0300

    удіу

--- js/script.js
+++ js/script.js
@@ -1,54 +1,98 @@
-'use strict'
-//Задание 1
-const city = 'Odessa';
-console.log(city);
-console.log(typeof city);
 
-const contry = 'Ukraine';
-console.log(contry);
-console.log(typeof contry);
 
-const populationOdessa = 1015826;
-console.log(populationOdessa);
-console.log(typeof populationOdessa);
+/*ветвления - вывод кода в консоль, после проверки условия.
+Если условие да то в консоле выведется код,
+если нет -то кода в консоде не будет 
+ if - если да то да 
+*/
 
-const stadium = true;
-console.log(stadium);
-console.log(typeof stadium);
+let firstName = 'Irina';
+if (29 < 31) {
+   console.log(firstName);
+}
+// if else - дефолтное выражение если условие не верно
 
-//Задание 2
-
-const width = 40;
-const heidth = 70;
-console.log(width * heidth);
-
-// Задание 3
-let time = 2;
-let speedOfFirst = 95;
-let speedOfSecond = 114;
-console.log((speedOfSecond * time) - (speedOfFirst * time));
-
-//Задание 4
-const randomNumber = Math.floor(Math.random() * 100);
-
-if ("randomNumber < 20") {
-   console.log("randomNumber меньше 20")
-} else if (randomNumber > 50) {
-   console.log("randomNumber больше 50")
+let lastName = 'Buhaenko';
+if (29 > 30) {
+   console.log(lastName);
 } else {
-   console.log("randomNumber больше 20, и меньше 50")
+   console.log('Irina Buhaenko')
 }
 
-//Задание 5
-let num;
-switch (randomNumber) {
-   case "randomNumber меньше 20":
-      num = randomNumber < 20;
-      break;
+// else if - проверка нескольких
+let myFamily = 'Buhaenko';
+if (29 > 30) {
+   console.log(myFamily);
+} else if (30 > 31) {
+   console.log('Irina Buhaenko')
+} else if (27 > 31) {
+   console.log(' Buhaenko Irina Sergeevna')
+} else {
+   console.log('Я вышла замуж и поменяла фамилию')
+}
 
-   case "randomNumber больше 50":
-      num = randomNumber > 50
+
+// cwitch
+let km = 14;
+switch (km) {
+   case 10:
+      console.log(10);
+      break;
+   case 11:
+      console.log(11);
+      break;
+   case 12:
+      console.log(12);
+      break;
+   case 13:
+      console.log(13);
+      break;
+   case 14:
+      console.log(14);
+      break;
+   case 15:
+      console.log(15);
       break;
    default:
-      "randomNumber больше 20, и меньше 50"
+      console.log('not')
 }
+
+
+/*Циклы - повторение одного и того н раз*/
+// while 
+
+let i = 0;
+while (i < 11) {
+   console.log(i);
+   i++;
+}
+
+// for
+for (let it = 10; it < 20; it++) {
+   console.log(it)
+}
+
+
+let w = 2;
+do {
+   console.log(w)
+   w++;
+} while (w > 3);
+
+// функции - выполняет действие 
+
+function sumChec(sum = 25, as = 55) {
+   console.log(sum + as);
+}
+sumChec()
+
+
+let num = function (a, b, c) {
+   console.log(a + b + c);
+}
+num(5, 19, 8957);
+
+let doc = (s, d) => {
+   console.log(s + d);
+}
+doc(2, 80)
