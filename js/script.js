ac93e7979a928e140215751cb3cf056ced470c17
Irina178 <Irina178@users.noreply.github.com>
2022-04-13 08:04:56 +0300

    arr

--- js/script.js
+++ js/script.js
@@ -1,98 +1,257 @@
 
+'use strict'
+//создание объекта
+// let student = {
+//    name: 'Irina',
+//    surname: 'Bugaenko',
+//    age: 30,
+//    address: {
+//       citu: 'Odessa',
+//       strect: 'Lugova',
+//    }
+// }
+// console.log(student);
+// console.log(student['age'])
+// console.log(student.address['citu']);
+// // добавление свойств 
+// student.vus = 'ODABA';
+// console.log(student)
+// const moonher = {
+//    namem: 'Julia',
+//    surnamem: 'Buhaenko',
+//    age: 45,
+//    addressm: {
+//       citum: 'Odessa',
+//       streect: 'Lugova',
+//    }
+// }
+// console.log(moonher);
+// console.log(moonher.age);
+// console.log(moonher.addressm['streect']);
+// // добавление свойств 
+// moonher.vus = 'KNUBA';
+// console.log(moonher);
+// let sister = {
+//    names: 'Julia',
+//    family: 'Alexsandrova',
+//    ade: 36,
+//    address: {
+//       city: 'Nikolaiv',
+//       strects: 'Mira',
+//    }
+// }
+// console.log(sister);
+// console.log(sister.address['strects']);
+// // добавление свойств 
+// sister.vus = 'SVAZ';
+// console.log(sister);
 
-/*ветвления - вывод кода в консоль, после проверки условия.
-Если условие да то в консоле выведется код,
-если нет -то кода в консоде не будет 
- if - если да то да 
-*/
+// let faanher = {
+//    name: 'Sergey',
+//    surname: 'Buhaenko',
+//    age: 47,
 
-let firstName = 'Irina';
-if (29 < 31) {
-   console.log(firstName);
-}
-// if else - дефолтное выражение если условие не верно
-
-let lastName = 'Buhaenko';
-if (29 > 30) {
-   console.log(lastName);
-} else {
-   console.log('Irina Buhaenko')
-}
-
-// else if - проверка нескольких
-let myFamily = 'Buhaenko';
-if (29 > 30) {
-   console.log(myFamily);
-} else if (30 > 31) {
-   console.log('Irina Buhaenko')
-} else if (27 > 31) {
-   console.log(' Buhaenko Irina Sergeevna')
-} else {
-   console.log('Я вышла замуж и поменяла фамилию')
-}
-
-
-// cwitch
-let km = 14;
-switch (km) {
-   case 10:
-      console.log(10);
-      break;
-   case 11:
-      console.log(11);
-      break;
-   case 12:
-      console.log(12);
-      break;
-   case 13:
-      console.log(13);
-      break;
-   case 14:
-      console.log(14);
-      break;
-   case 15:
-      console.log(15);
-      break;
-   default:
-      console.log('not')
-}
+// }
+// console.log(faanher);
 
 
-/*Циклы - повторение одного и того н раз*/
-// while 
+// // добавления объкта внутырь объекта 
+// let megasine = {
+//    bread: 1,
+//    milk: 2,
+// }
+// console.log(megasine);
+// megasine.products = {
+//    shampoo: 2,
+//    balm: 1
+// }
+// console.log(megasine);
+// console.log(typeof (megasine.bread))
+// delete megasine.products.shampoo;
+// console.log(megasine)
+// let baser = {
+//    mane: 'Klavdia',
+// }
+// console.log(baser);
+// baser.address = {
+//    citu: ' Revovo',
+//    strect: 'Girna',
+// }
+// console.log(baser)
+// delete baser.address;
+// console.log(baser)
 
-let i = 0;
-while (i < 11) {
-   console.log(i);
-   i++;
-}
+// let diser = {
+//    name: 'Nikola',
+//    surname: 'Ivanov',
+// }
+// console.log(diser)
 
-// for
-for (let it = 10; it < 20; it++) {
-   console.log(it)
-}
+// diser.address = {
+//    citu: "Peterbugr",
+//    age: 80,
+// }
+// console.log(diser)
+// diser.age = 82;
+// console.log(diser)
+
+//копирование обхектов
+// let hom = {
+//    citu: 'Odessa',
+//    rain: 'Malina',
+//    strect: 'Lugova',
+//    numder: 27,
+// }
+// console.log(hom);
+// let homTwoo = hom;
+// homTwoo.numder = 32;
+// console.log(hom);
+// console.log(homTwoo);
 
 
-let w = 2;
-do {
-   console.log(w)
-   w++;
-} while (w > 3);
+// let circl = {
+//    one: 2,
+//    twoo: 4,
+// }
+// console.log(circl);
+// let circlTwoo = circl;
+// circlTwoo.one = 8;
 
-// функции - выполняет действие 
+// console.log(circl);
+// circlTwoo.three = 25;
+// console.log(circlTwoo);
 
-function sumChec(sum = 25, as = 55) {
-   console.log(sum + as);
-}
-sumChec()
+// let apartmens = {
+//    mather: 1,
+//    faser: 1,
+//    sister: 1,
+// }
+// console.log(apartmens)
+// let apartmensTwoo = apartmens;
+// apartmensTwoo.doc = 1;
+// apartmensTwoo.cet = 3;
+// apartmensTwoo.iam = 1;
+// console.log(apartmensTwoo);
+// console.log(apartmens);
+// delete apartmens.sister;
+// console.log(apartmens);
+
+// // дублирование объекта
+// let megasine = {
+//    product: 100,
+//    colors: 500,
+//    cans: 120,
+// }
+// console.log(megasine);
+// let megasineOtd = Object.assign({}, megasine)
+// console.log(megasineOtd);
+
+// let arbaitenKyiv = {
+
+//    twoo: 'KNUBA',
+//    three: 'KP',
+//    Four: 'ABS',
+// }
+
+// console.log(arbaitenKyiv);
+// let arbaitenOdessa = Object.assign({}, arbaitenKyiv);
+// console.log(arbaitenOdessa);
+// arbaitenOdessa.one = 'OGASA';
+// delete arbaitenOdessa.twoo;
+// delete arbaitenOdessa.three;
+// delete arbaitenOdessa.Four;
+// console.log(arbaitenOdessa)
+
+// const tehnicks = {
+//    tv: 3,
+//    notBuce: 2
+// }
+// let newTehnics = Object.assign({}, tehnicks)
+// console.log(newTehnics);
+// console.log(tehnicks);
+
+// let one = {
+//    a: 1,
+//    s: 3,
+// }
+// console.log(one);
+// for (let key in one) {
+//    console.log(key)
+//    console.log(one[key])
+// }
+/**Массив — структура данных для хранения и манипулирования коллекцией индексированных значений.
+ Используются для хранения упорядоченных коллекций данных, 
+ например списка курортов, товаров, клиентов в отеле и т. п. */
+//Сохдания массива
+let arr = [
+   'Irina',
+   'Julia',
+   'jeck',
+   'Alex',
+]
+console.log(arr);
+console.log(arr[2]);
+//длина массива
+console.log(arr.length);
+//копирование массива - агадлгтично копии объекта
+let array = [
+
+   'jeck',
+   'Alex',
+]
+
+let newArr = array;
+console.log(newArr);
+console.log(array);
+//укоротить массив
+let ms = [
+
+   'jeck',
+   'Alex',
+]
+console.log(ms)
+ms.length = 1
+console.log(ms)
+// Изменить знвчения 
 
 
-let num = function (a, b, c) {
-   console.log(a + b + c);
-}
-num(5, 19, 8957);
+let arname = [
+   'Ваня',
+   ' Петя',
+   'Коля',
+]
+console.log(arname);
+arname[1] = 'Василиса';
+console.log(arname)
+// Добавить новое значение в массив
+arname[3] = 'Алекс';
+console.log(arname)
+// МЕТОДЫ МАССИВОВ
+// Метод push - добавляет элемент в конец массива
+let ar = ['Ваня', 'Иштван', 'Оля',];
+ar.push('Вася');
+console.log(ar);
 
-let doc = (s, d) => {
-   console.log(s + d);
-}
-doc(2, 80)
+ar.push('Дима', 'Катя');
+console.log(ar);
+// Метод shift - удаляет элемент в начале,
+// так что второй элемент становится первым.
+let a = ['Ваня', 'Иштван', 'Оля',];
+a.shift();
+console.log(a);
+// Метод pop  - удаляет последний элемент
+
+let r = ['Ваня', 'Иштван', 'Оля',];
+r.pop();
+console.log(r);
+// Метод unshift  - добавляет элемент в начало массива
+
+let rr = ['Ваня', 'Иштван', 'Оля',];
+rr.unshift('Вася');
+console.log(rr);
+
+rr.unshift('Дима', 'Катя');
+console.log(rr);
+
+
+
